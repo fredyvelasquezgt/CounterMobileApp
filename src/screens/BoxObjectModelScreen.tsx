0092c8058eeb532d5fdc4c446f862f0c62430c40
Fredy Velasquez <macacoloco696@gmail.com>
2022-08-20 16:07:40 -0600

    WIP: BoxObjectModelScreen created

---
+++ src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const BoxObjectModelScreen = () => {
+    return (
+        <View
+        >
+
+            <Text>
+                Box Object Model
+            </Text>
+            
+        </View
+        >
+    )
+}
