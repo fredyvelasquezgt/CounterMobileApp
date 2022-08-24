22570d4f189ed8aac59577e49ab4f6a1872a9d21
Fredy Velasquez <macacoloco696@gmail.com>
2022-08-24 10:36:49 -0600

    useDimensions hook used

--- src/screens/DimensionesScreen.tsx
+++ src/screens/DimensionesScreen.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, useWindowDimensions } from 'react-native';
 
-const {width , height} = Dimensions.get('window')
+//const {width , height} = Dimensions.get('window')
+
+
 
 export const DimensionesScreen = () => {
+    const {width, height} = useWindowDimensions();
+
     return (
         <View>
             <View style={styles.container}>
-                <View style={styles.cajaMorada}/>
+                <View style={{...styles.cajaMorada, width: width * 0.50}}/>
                 <View style={styles.cajaNaranja}/>
             </View>
                 <Text style={styles.title}>W: {width}, H:{height}</Text>
         </View>
-        
-
-
-
+    
     );
 };
 
